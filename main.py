a23e82d07582e9e1470257f7e109a1384726fa98
Subha272001 <112246314+Subha272001@users.noreply.github.com>
2022-10-11 16:42:26 +0530

    snake_game_project

---
+++ main.py
@@ -0,0 +1,44 @@
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("--Snake Game--")
+screen.tracer(0)
+
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+is_game_on = True
+while is_game_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move_square()
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend_square()
+        scoreboard.increase_score()
+
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        scoreboard.reset()
+        snake.reset()
+
+    for square in snake.squares:
+        if square == snake.head:
+            pass
+        elif snake.head.distance(square) < 15:
+            scoreboard.reset()
+            snake.reset()
+screen.exitonclick()
